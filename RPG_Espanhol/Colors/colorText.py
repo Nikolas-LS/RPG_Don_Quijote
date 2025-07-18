5a72222aea63ebee2c35bdf6b67c7811e3ead4c8
Nikolas Lopes da Silva <nikolaslopess20@gmail.com>
2025-07-17 22:32:33 -0300

    Update colorText.py

---
+++ RPG_Espanhol/Colors/colorText.py
@@ -0,0 +1,29 @@
+# Cores para utilizar ao decorrer do programa
+
+# Cor vermelha
+def red(text):
+    return f"\033[0;31m{text}\033[m"
+
+# Cor verde
+def green(text):
+    return f"\033[0;32m{text}\033[m"
+
+# Cor amarela
+def yellow(text):
+    return f"\033[0;33m{text}\033[m"
+
+# Cor azul
+def blue(text):
+    return f"\033[0;34m{text}\033[m"
+
+# Cor roxo
+def purple(text):
+    return f"\033[0;35m{text}\033[m"
+
+# Cor azul claro
+def lightBlue(text):
+    return f"\033[0;36m{text}\033[m"
+
+# Cor cinza
+def gray(text):
+    return f"\033[0;37m{text}\033[m"
